6d0de2999231b26ce59e71cd0faa2f9d9f4d36b0
Kevindoblea <kevin.alvarenga93@gmail.com>
2023-03-21 21:56:58 -0600

    clas1, and clas2 added

---
+++ circle/circle/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenido, Calculemos circulos");
+//the area of circle is pi * radius * radius
+Console.WriteLine("Por favor ingresa el valor del radio");
+double radius = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El resultado del area del circulo es: " +  radius * radius * Math.PI);
---
+++ clase2/clase2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//program to calculate the area of a rectangle
+//we have to use the reserve word "var"
+var sideA = 0.0;
+var sideB = 0.0;
+Console.WriteLine("Calcula el area de un rectangulo");
+Console.WriteLine("Ingrese el valor del lago A");
+sideA = Convert.ToDouble(Console.ReadLine()) ;
+Console.WriteLine("Ingrese el valor del lado B");
+sideB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n");
+Console.WriteLine("El area de un rectangulo es: " + sideA * sideB);
+ 
